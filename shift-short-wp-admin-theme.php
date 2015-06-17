b3c1abc3c10b8668e9657a8493c49c9dab378a04
brshift <brshift@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-17 13:18:12 +0000

    add short_ before some filters
    git-svn-id: https://plugins.svn.wordpress.org/shift-short-wp-admin-theme/trunk@1182567 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shift-short-wp-admin-theme.php
+++ shift-short-wp-admin-theme.php
@@ -34,26 +34,26 @@ add_action( 'login_enqueue_scripts', 'short_theme_login' );
 function short_theme_login() {
     wp_enqueue_style( 'short-login', plugins_url('css/login.css', __FILE__) );
 }
-function login_url_logo() {
+
+add_filter( 'login_headerurl', 'short_login_url_logo' );
+function short_login_url_logo() {
     return home_url();
 }
-add_filter( 'login_headerurl', 'login_url_logo' );
 
-function login_logo_url_title() {
+add_filter( 'login_headertitle', 'short_login_logo_url_title' );
+function short_login_logo_url_title() {
     return the_title();
 }
-add_filter( 'login_headertitle', 'login_logo_url_title' );
+
 
 // WP ADMIN
 add_action( 'admin_enqueue_scripts', 'short_theme_general' );
-
 function short_theme_general() {
         wp_enqueue_style( 'short-general', plugins_url('css/general.css', __FILE__) );
 }
 
 // CUSTOMIZE
 add_action( 'admin_enqueue_scripts', 'short_theme_customize' );
-
 function short_theme_customize() {
     global $pagenow;
 	if (( $pagenow == 'customize.php' )) {
@@ -63,7 +63,6 @@ function short_theme_customize() {
 
 // WP ADMIN BAR
 add_action( 'wp_head', 'short_theme_bar' );
-
 function short_theme_bar() {
 	if ( is_user_logged_in() ) {
         wp_enqueue_style( 'short-bar', plugins_url('css/admin-bar.css', __FILE__) );

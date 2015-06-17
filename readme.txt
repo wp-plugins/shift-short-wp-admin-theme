b7f1202d61f8fec369763f0e60e951af11e47492
brshift <brshift@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-17 13:12:06 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/shift-short-wp-admin-theme/trunk@1182564 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,16 @@
+=== SHIFT Short WP Admin Theme ===
+Contributors: brshift
+Requires at least: 4.1
+Tested up to: 4.2.2
+Stable tag: 4.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Just another wp admin theme. Simple but clean.
+
+== Installation ==
+
+1. Upload the entire `shift-short-wp-admin-theme` folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+You will not find any new page, just the visual itself will be changed.
---
+++ shift-short-wp-admin-theme.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Plugin Name: SHIFT Short WP Admin Theme
+Plugin URI: https://brshift.com/
+Description: Clean the WordPress Dashboard and get more space!
+Version: 1.0
+Author: SHIFT
+Author URI: https://brshift.com/
+License: GPLv2
+*/
+/*
+ *      Copyright 2015 Shift Empreendimentos Virtuais <producao@brshift.com>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+?>
+<?php
+
+// LOGIN PAGE
+add_action( 'login_enqueue_scripts', 'short_theme_login' );
+function short_theme_login() {
+    wp_enqueue_style( 'short-login', plugins_url('css/login.css', __FILE__) );
+}
+function login_url_logo() {
+    return home_url();
+}
+add_filter( 'login_headerurl', 'login_url_logo' );
+
+function login_logo_url_title() {
+    return the_title();
+}
+add_filter( 'login_headertitle', 'login_logo_url_title' );
+
+// WP ADMIN
+add_action( 'admin_enqueue_scripts', 'short_theme_general' );
+
+function short_theme_general() {
+        wp_enqueue_style( 'short-general', plugins_url('css/general.css', __FILE__) );
+}
+
+// CUSTOMIZE
+add_action( 'admin_enqueue_scripts', 'short_theme_customize' );
+
+function short_theme_customize() {
+    global $pagenow;
+	if (( $pagenow == 'customize.php' )) {
+        wp_enqueue_style( 'short-customize', plugins_url('css/customize.css', __FILE__) );
+	}
+}
+
+// WP ADMIN BAR
+add_action( 'wp_head', 'short_theme_bar' );
+
+function short_theme_bar() {
+	if ( is_user_logged_in() ) {
+        wp_enqueue_style( 'short-bar', plugins_url('css/admin-bar.css', __FILE__) );
+	}
+}
